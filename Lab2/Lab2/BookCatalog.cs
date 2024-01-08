aa1eb7369a46a14d654768db998b82ba3d40dbf3
v01dARG <126291704+v01dARG@users.noreply.github.com>
2024-01-08 14:01:23 +0300

    Add files via upload

---
+++ Lab2/Lab2/BookCatalog.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal struct Book // Структура для хранения книг
+{
+    public String Title;// название
+    public String Author;// автор
+    public String Description;// описание
+    public String ISBN;// ISBN номер с дефисами 978-5-699-12014-7
+    public Book(String Title, String Author, String Description, String ISBN)
+    {
+        this.Title = Title;
+        this.Author = Author;
+        this.Description = Description;
+        this.ISBN = ISBN;
+    }
+}
+
+public struct FoundBook : IComparable// Структура для хранения найденной книги
+{
+    public String Title;// название
+    public String Author;// автор
+    public String Description;// описание
+    public int KeyNum;// количество найденных ключевых слов
+    public FoundBook(String Title, String Author, String Description)
+    {
+        this.Title = Title;
+        this.Author = Author;
+        this.Description = Description;
+        this.KeyNum = 0;
+    }
+    public FoundBook(String Title, String Author, String Description, int KeyNum)
+    {
+        this.Title = Title;
+        this.Author = Author;
+        this.Description = Description;
+        this.KeyNum = KeyNum;
+    }
+    public int CompareTo(object obj)
+    {
+        FoundBook it = (FoundBook)obj;
+        if (this.KeyNum == it.KeyNum) return 0;
+        else if (this.KeyNum > it.KeyNum) return -1;
+        else return 1;
+    }
+}
+
+public class BookCatalog
+{
+    private static List<Book> BookList = new List<Book>();//Список книг
+    // Добавить книгу
+    public bool AddBook(String Title, String Author, String Description, String ISBN)
+    {
+        if (Title.Length == 0 || Author.Length == 0)
+        {
+            return false;
+        }
+        else
+        {
+            BookList.Add(new Book(Title, Author, Description, ISBN));
+            return true;
+        }
+    }
+    // Найти книги по названию
+    public bool SelectBooksByTitle(String Title, ref List<FoundBook> Result)
+    {
+        Result.Clear();
+        if (Title.Length == 0)
+        {
+            return false;
+        }
+        Title = Title.ToLower();
+        foreach (Book book in BookList) { 
+            if (book.Title.ToLower() == Title)
+            {
+                Result.Add(new FoundBook(book.Title, book.Author, book.Description));
+            }
+        }
+        return (Result.Count > 0);
+    }
+    // Найти книги по части названия
+    public bool SelectBooksByPartOfTitle(String Title, ref List<FoundBook> Result)
+    {
+        Result.Clear();
+        if (Title.Length == 0)
+        {
+            return false;
+        }
+        Title = Title.ToLower();
+        foreach (Book book in BookList)
+        {
+            if (book.Title.ToLower().IndexOf(Title)>=0)
+            {
+                Result.Add(new FoundBook(book.Title, book.Author, book.Description));
+            }
+        }
+        return (Result.Count > 0);
+    }
+    // Найти книги по автору
+    public bool SelectBooksByAuthor(String Author, ref List<FoundBook> Result)
+    {
+        Result.Clear();
+        if (Author.Length == 0)
+        {
+            return false;
+        }
+        Author = Author.ToLower();
+        foreach (Book book in BookList)
+        {
+            if (book.Author.ToLower() == Author)
+            {
+                Result.Add(new FoundBook(book.Title, book.Author, book.Description));
+            }
+        }
+        return (Result.Count > 0);
+    }
+    // Найти книги по ключевым словам (через пробел)
+    public bool SelectBooksByKeyWords(String StrKeyWords, ref List<FoundBook> Result)
+    {
+        Result.Clear();
+        if (StrKeyWords.Length == 0)
+        {
+            return false;
+        }
+        StrKeyWords = StrKeyWords.ToLower();
+        String[] KeyWords = StrKeyWords.Split(' ');
+        if (KeyWords.Length == 0)
+        {
+            return false;
+        }
+        foreach (Book book in BookList)
+        {
+            int KeyNum = 0;// количество найденных ключевых слов
+            String TitleLower = book.Title.ToLower();
+            String DescLower = book.Description.ToLower();
+            foreach (String key in KeyWords)
+            {
+                if (TitleLower.IndexOf(key + ' ') >= 0)
+                {
+                    KeyNum++;
+                }
+                else if (TitleLower.IndexOf(key + '.') >= 0)
+                {
+                    KeyNum++;
+                }
+                else if (TitleLower.IndexOf(key + ',') >= 0)
+                {
+                    KeyNum++;
+                }
+                else if (TitleLower.EndsWith(key))
+                {
+                    KeyNum++;
+                }
+                else if (DescLower.IndexOf(key + ' ') >= 0)
+                {
+                    KeyNum++;
+                }
+                else if (DescLower.IndexOf(key + '.') >= 0)
+                {
+                    KeyNum++;
+                }
+                else if (DescLower.IndexOf(key + ',') >= 0)
+                {
+                    KeyNum++;
+                }
+                else
+                if (DescLower.EndsWith(key))
+                {
+                    KeyNum++;
+                }
+            }
+            if (KeyNum > 0)// Добавим в результат
+            {
+                Result.Add(new FoundBook(book.Title, book.Author, book.Description, KeyNum));
+            }
+        }
+        Result.Sort();
+        return (Result.Count > 0);
+    }
+}
---
+++ Lab2/Lab2/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void WriteMenu()
+        {
+            Console.WriteLine("Книжный каталог:");
+            Console.WriteLine("1. Добавить книгу в каталог");
+            Console.WriteLine("2. Поиск по названию");
+            Console.WriteLine("3. Поиск по фрагменту названия");
+            Console.WriteLine("4. Поиск по имени автора");
+            Console.WriteLine("5. Поиск по ключевым словам");
+            Console.WriteLine("6. Выход");
+        }
+        static void Main(string[] args)
+        {
+            BookCatalog Catalog = new BookCatalog();//Объект Книжный каталог
+            List<FoundBook> FoundBooks = new List<FoundBook>();//найденые книги
+            String S;//строка для ввода
+            bool exit = false;
+            WriteMenu();
+            while (!exit)
+            {
+                S = Console.ReadLine();
+                if (S.Length == 1)
+                {
+                    switch (S[0])
+                    {
+                        case '1':
+                            Console.WriteLine("Введите название книги");
+                            String STitle = Console.ReadLine();
+                            Console.WriteLine("Введите автора книги");
+                            String SAuthor = Console.ReadLine();
+                            Console.WriteLine("Введите номер ISBN");
+                            String ISBN = Console.ReadLine();
+                            Console.WriteLine("Введите краткое описание");
+                            S = Console.ReadLine();
+                            if (Catalog.AddBook(STitle, SAuthor, S, ISBN))
+                            {
+                                Console.WriteLine("Книга добавлена " + STitle+" "+SAuthor);
+                            }   
+                            else
+                            {
+                                Console.WriteLine("Книга не добавлена!");
+                            }
+                            WriteMenu();
+                            break;
+                        case '2':
+                            Console.WriteLine("Введите название книги");
+                            S = Console.ReadLine();
+                            if (Catalog.SelectBooksByTitle(S, ref FoundBooks))
+                            {
+                                for (int i = 0; i < FoundBooks.Count; i++)
+                                {
+                                    Console.WriteLine(Convert.ToString(i + 1) + ". " + FoundBooks[i].Title + " " + FoundBooks[i].Author);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Таких книг не найдено!");
+                            }
+                            WriteMenu();
+                            break;
+                        case '3':
+                            Console.WriteLine("Введите фрагмент названия книги");
+                            S = Console.ReadLine();
+                            if (Catalog.SelectBooksByPartOfTitle(S, ref FoundBooks))
+                            {
+                                for (int i = 0; i < FoundBooks.Count; i++)
+                                {
+                                    Console.WriteLine(Convert.ToString(i + 1) + ". " + FoundBooks[i].Title + " " + FoundBooks[i].Author);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Таких книг не найдено!");
+                            }
+                            WriteMenu();
+                            break;
+                        case '4':
+                            Console.WriteLine("Введите автора книги");
+                            S = Console.ReadLine();
+                            if (Catalog.SelectBooksByAuthor(S, ref FoundBooks))
+                            {
+                                for (int i = 0; i < FoundBooks.Count; i++)
+                                {
+                                    Console.WriteLine(Convert.ToString(i + 1) + ". " + FoundBooks[i].Title + " " + FoundBooks[i].Author);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Таких книг не найдено!");
+                            }
+                            WriteMenu();
+                            break;
+                        case '5':
+                            Console.WriteLine("Введите ключевые слова через пробел");
+                            S = Console.ReadLine();
+                            if (Catalog.SelectBooksByKeyWords(S, ref FoundBooks))
+                            {
+                                for (int i = 0; i < FoundBooks.Count; i++)
+                                {
+                                    Console.WriteLine(Convert.ToString(i + 1) + ". " + FoundBooks[i].Title + " " + FoundBooks[i].Author + " " + FoundBooks[i].Description);
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Таких книг не найдено!");
+                            }
+                            WriteMenu();
+                            break;
+                        case '6':
+                            exit = true;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
